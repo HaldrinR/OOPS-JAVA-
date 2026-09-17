4ac288090d1a64cdcc1c7a18b0872c03d221bee1
Haldrin R <haldrinrajan@gmail.com>
2026-09-17 15:50:01 +0530

    changed codes

--- Practice/Files.java
+++ Practice/Files.java
@@ -1,22 +1,29 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.FileNotFoundException;
-import java.util.Scanner;
+// import java.io.File;
+// import java.io.FileWriter;
+// import java.io.IOException;
+// import java.io.FileNotFoundException;
+// import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 public class Files{
     public static void main(String[]args){
         try{
-            File obj = new File("santa.txt");
-            Scanner Reader = new Scanner(obj);
-            while(Reader.hasNextLine()){
-                String data = Reader.nextLine();
-                System.out.println(data);
-            }
+            FileReader obj = new FileReader("santa.txt");
             FileWriter Writer = new FileWriter("clause.txt");
-            Writer.write("Good morning Sunshine!!!!");
+            int a;
+            while((a=obj.read())!=-1){
+                Writer.write(a);
+            }
+            // Scanner Reader = new Scanner(obj);
+            // while(Reader.hasNextLine()){
+                // String data = Reader.nextLine();
+                // System.out.println(data);
+            // }
+            
+            // Writer.write("Good morning Sunshine!!!!");
             Writer.close();
             System.out.println("Successfully written:)");
-            Reader.close();
+            obj.close();
             
         }
         catch(FileNotFoundException e){
